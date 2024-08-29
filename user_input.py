6d62673064864fbfda43848c7607dac27df3e59c
Mica <132666987+Barasa-Micah@users.noreply.github.com>
2024-08-29 17:47:49 +0300

    Add user_input with personalized message

---
+++ user_input.py
@@ -0,0 +1,9 @@
+name = input("Enter username:")
+#promts one to enter username
+
+age = input("How old are you?")
+
+location = input("Enter your current location:")
+
+# Print a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
